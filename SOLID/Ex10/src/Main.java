f446d7793a93ab3319a09b0dc70749e50c184c09
aagam3145 <aagam.24bcs10052@sst.scaler.com>
2026-03-02 18:19:32 +0530

    Refactor Ex7-Ex10: ISP and DIP SOLID principles

--- SOLID/Ex10/src/Main.java
+++ SOLID/Ex10/src/Main.java
@@ -2,7 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("=== Transport Booking ===");
         TripRequest req = new TripRequest("23BCS1010", new GeoPoint(12.97, 77.59), new GeoPoint(12.93, 77.62));
-        TransportBookingService svc = new TransportBookingService();
+
+        DistanceService distService = new DistanceCalculator();
+        AllocationService allocService = new DriverAllocator();
+        PaymentService payService = new PaymentGateway();
+
+        TransportBookingService svc = new TransportBookingService(distService, allocService, payService);
         svc.book(req);
     }
 }
--- SOLID/Ex7/src/ClassroomController.java
+++ SOLID/Ex7/src/ClassroomController.java
@@ -1,27 +1,29 @@
 public class ClassroomController {
     private final DeviceRegistry reg;
 
-    public ClassroomController(DeviceRegistry reg) { this.reg = reg; }
+    public ClassroomController(DeviceRegistry reg) {
+        this.reg = reg;
+    }
 
     public void startClass() {
-        SmartClassroomDevice pj = reg.getFirstOfType("Projector");
+        Powerable pj = (Powerable) reg.getFirstByName("Projector");
         pj.powerOn();
-        pj.connectInput("HDMI-1");
+        ((InputConnectable) reg.getFirstByName("Projector")).connectInput("HDMI-1");
 
-        SmartClassroomDevice lights = reg.getFirstOfType("LightsPanel");
+        BrightnessControllable lights = (BrightnessControllable) reg.getFirstByName("LightsPanel");
         lights.setBrightness(60);
 
-        SmartClassroomDevice ac = reg.getFirstOfType("AirConditioner");
+        TemperatureControllable ac = (TemperatureControllable) reg.getFirstByName("AirConditioner");
         ac.setTemperatureC(24);
 
-        SmartClassroomDevice scan = reg.getFirstOfType("AttendanceScanner");
+        Scanner scan = (Scanner) reg.getFirstByName("AttendanceScanner");
         System.out.println("Attendance scanned: present=" + scan.scanAttendance());
     }
 
     public void endClass() {
         System.out.println("Shutdown sequence:");
-        reg.getFirstOfType("Projector").powerOff();
-        reg.getFirstOfType("LightsPanel").powerOff();
-        reg.getFirstOfType("AirConditioner").powerOff();
+        ((Powerable) reg.getFirstByName("Projector")).powerOff();
+        ((Powerable) reg.getFirstByName("LightsPanel")).powerOff();
+        ((Powerable) reg.getFirstByName("AirConditioner")).powerOff();
     }
 }
